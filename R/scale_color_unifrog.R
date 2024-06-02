d95a80e51160f2f7d00536128b3fb0ec3cd636c7
Ishmael R <ishmael@unifrog.org>
2024-06-02 20:11:23 +0100

    added colour blind friendly palettes

--- R/scale_color_unifrog.R
+++ R/scale_color_unifrog.R
@@ -5,6 +5,7 @@
 #' `scale_color_unifrog_c` for a *continuous* scale.
 #'
 #' @inheritParams palette_unifrog
+#' @inheritParams cb_palettes
 #' @inheritParams unifrog_palettes
 #'
 #' @param discrete Boolean indicating whether color aesthetic is discrete or not.
@@ -49,7 +50,7 @@ scale_color_unifrog <-
       if (discrete) {
         discrete_scale(
           aesthetics = aesthetics,
-          paste0("unifrog_", palette),
+          #paste0("unifrog_", palette),
           palette = pal,
           ...
         )
@@ -141,7 +142,7 @@ scale_fill_unifrog <-
       if (discrete) {
         discrete_scale(
           aesthetics = aesthetics,
-          paste0("unifrog_", palette),
+          #paste0("unifrog_", palette),
           palette = pal,
           ...
         )
@@ -346,8 +347,194 @@ unifrog_palettes <- list(
   "likert7" = likert7
 )
 
+#' Colour-Blind Friendly Qualitative Bright Palette
+#' @import unikn
+#' @export
+#' @examples
+#' unikn::seecol(qual_bright, main = "qual_bright")
+qual_bright <-
+  unikn::newpal(
+    col = c('#4477AA', '#EE6677', '#228833', '#CCBB44', '#66CCEE', '#AA3377', '#BBBBBB'),
+    names = c("blue", "cyan", "green", "yellow", "red", "purple", "grey"),
+    as_df = F
+  )
+
+#' Colour-Blind Friendly Qualitative High-Contrast Palette
+#' @import unikn
+#' @export
+#' @examples
+#' unikn::seecol(qual_hc, main = "qual_hc")
+qual_hc <-
+  unikn::newpal(
+    col = c('#004488', '#DDAA33', '#BB5566'),
+    names = c("blue","yellow","red"),
+    as_df = F
+  )
+
+#' Colour-Blind Friendly Qualitative Vibrant Palette
+#' @import unikn
+#' @export
+#' @examples
+#' unikn::seecol(qual_vibrant, main = "qual_vibrant")
+qual_vibrant <-
+  unikn::newpal(
+    col = c('#EE7733', '#0077BB', '#33BBEE', '#EE3377', '#CC3311', '#009988', '#BBBBBB'),
+    names = c("orange", "blue", "cyan", "magenta", "red", "teal", "grey"),
+    as_df = F
+  )
+
+#' Colour-Blind Friendly Qualitative Muted Palette
+#' @import unikn
+#' @export
+#' @examples
+#' unikn::seecol(qual_muted, main = "qual_muted")
+qual_muted <-
+  unikn::newpal(
+    col = c( '#CC6677', '#332288', '#DDCC77', '#117733', '#88CCEE', '#882255', '#44AA99', '#999933', '#AA4499'),
+    names = c("rose", "indigo", "sand", "green","cyan","wine",
+              "teal", "olive", "purple"),
+    as_df = F
+  )
+
+#' Colour-Blind Friendly Qualitative Medium Contrast Palette
+#' @import unikn
+#' @export
+#' @examples
+#' unikn::seecol(qual_mc, main = "qual_mc")
+qual_mc <-
+  unikn::newpal(
+    col = c(  '#6699CC', '#004488', '#EECC66', '#994455', '#997700', '#EE99AA'),
+    names = c("light blue", "dark blue", "light yellow", "dark red", "dark yellow", "dark yellow"),
+    as_df = F
+  )
+
+#' Colour-Blind Friendly Qualitative Pale Palette
+#' @import unikn
+#' @export
+#' @examples
+#' unikn::seecol(qual_pale, main = "qual_pale")
+qual_pale <-
+  unikn::newpal(
+    col = c('#BBCCEE', '#CCEEFF', '#CCDDAA', '#EEEEBB', '#FFCCCC', '#DDDDDD'),
+    names = c("pale blue", "pale cyan","pale green","pale yellow",
+              "pale red", "pale grey"),
+    as_df = F
+  )
+
+#' Colour-Blind Friendly Qualitative Light Palette
+#' @import unikn
+#' @export
+#' @examples
+#' unikn::seecol(qual_light, main = "qual_light")
+qual_light <-
+  unikn::newpal(
+    col = c( '#77AADD', '#EE8866', '#EEDD88', '#FFAABB', '#99DDFF', '#44BB99', '#BBCC33', '#AAAA00', '#DDDDDD'),
+    names = c("light blue", "orange","light yellow", "pink", "light cyan",
+              "mint","pear","olive", "pale grey"),
+    as_df = F
+  )
+
+#' Colour-Blind Friendly Diverging Sunset Palette
+#' @import unikn
+#' @export
+#' @examples
+#' unikn::seecol(div_sunset, main = "div_sunset")
+div_sunset <-
+  unikn::newpal(
+    col = c( '#364B9A', '#4A7BB7', '#6EA6CD', '#98CAE1', '#C2E4EF', '#EAECCC', '#FEDA8B', '#FDB366', '#F67E4B', '#DD3D2D', '#A50026'),
+    as_df = F
+  )
+
+#' Colour-Blind Friendly Diverging Purple-Green Palette
+#' @import unikn
+#' @export
+#' @examples
+#' unikn::seecol(div_PrGn, main = "div_PrGn")
+div_PrGn <-
+  unikn::newpal(
+    col = c( '#762A83', '#9970AB', '#C2A5CF', '#E7D4E8', '#F7F7F7', '#D9F0D3', '#ACD39E', '#5AAE61', '#1B7837'),
+    as_df = F
+  )
+
+#' Colour-Blind Friendly Diverging Nightfall Palette
+#' @import unikn
+#' @export
+#' @examples
+#' unikn::seecol(div_nightfall, main = "div_nightfall")
+div_nightfall <-
+  unikn::newpal(
+    col = c('#125A56', '#00767B', '#238F9D', '#42A7C6', '#60BCE9', '#9DCCEF', '#C6DBED', '#DEE6E7', '#ECEADA', '#F0E6B2', '#F9D576', '#FFB954', '#FD9A44', '#F57634', '#E94C1F', '#D11807', '#A01813'),
+    as_df = F
+  )
+
+#' Colour-Blind Friendly Diverging Blue-Red Palette
+#' @import unikn
+#' @export
+#' @examples
+#' unikn::seecol(div_BuRd, main = "div_BuRd")
+div_BuRd <-
+  unikn::newpal(
+    col = c(  '#2166AC', '#4393C3', '#92C5DE', '#D1E5F0', '#F7F7F7', '#FDDBC7', '#F4A582', '#D6604D', '#B2182B'),
+    as_df = F
+  )
 
 
+#' Colour-Blind Friendly Sequential Yellow-Brown Palette
+#' @import unikn
+#' @export
+#' @examples
+#' unikn::seecol(seq_YlBr, main = "seq_YlBr")
+seq_YlBr <-
+  unikn::newpal(
+    col = c( '#FFFFE5', '#FFF7BC', '#FEE391', '#FEC44F', '#FB9A29', '#EC7014', '#CC4C02', '#993404', '#662506'),
+    as_df = F
+  )
+
+
+#' Colour-Blind Friendly Sequential Iridescent Palette
+#' @import unikn
+#' @export
+#' @examples
+#' unikn::seecol(seq_iridescent, main = "seq_iridescent")
+seq_iridescent <-
+  unikn::newpal(
+    col = c('#FEFBE9', '#FCF7D5', '#F5F3C1', '#EAF0B5', '#DDECBF', '#D0E7CA', '#C2E3D2', '#B5DDD8', '#A8D8DC', '#9BD2E1', '#8DCBE4', '#81C4E7', '#7BBCE7', '#7EB2E4', '#88A5DD', '#9398D2', '#9B8AC4', '#9D7DB2', '#9A709E', '#906388', '#805770', '#684957', '#46353A'),
+    as_df = F
+  )
+
+
+#' Colour-Blind Friendly Sequential Incandescent Palette
+#' @import unikn
+#' @export
+#' @examples
+#' unikn::seecol(seq_incandescent, main = "seq_incandescent")
+seq_incandescent <-
+  unikn::newpal(
+    col = c('#CEFFFF', '#C6F7D6', '#A2F49B', '#BBE453', '#D5CE04', '#E7B503', '#F19903', '#F6790B', '#F94902', '#E40515', '#A80003'),
+    as_df = F
+  )
+
+#' List of all Colour-Blind Friendly palettes
+#' @import unikn
+#' @export
+#' @examples
+#' library(unikn)
+#' unikn::seecol(cb_palettes, main = "All CB-friendly Palettes", pal_names = names(cb_palettes))
+cb_palettes <- list(
+  "qual_bright" = qual_bright,
+  "qual_vibrant\nbad data DDDDDD" = qual_vibrant,
+  "qual_hc" = qual_hc,
+  "qual_mc\nuse for pairs" = qual_mc,
+  "qual_pale\n cell bg" = qual_pale,
+  "qual_light\n cell bg" = qual_light,
+  "div_sunset\n bad data white" = div_sunset,
+  "div_nightfall\n bad data white" = div_nightfall,
+  "div_PrGn\n bad data FFEE99" = div_PrGn,
+  "div_BuRd\n bad data FFEE99" = div_BuRd,
+  "seq_YlBr\n bad data 888888" = seq_YlBr,
+  "seq_iridescent\n bad data 999999" = seq_iridescent,
+  "seq_incandescent\n bad data 888888" = seq_incandescent
+)
 
 
 #' Unifrog colour palette
@@ -367,9 +554,11 @@ palette_unifrog <-
   function(palette = "unifrog_main",
            reverse = FALSE,
            ...) {
-    .retrieve_palette(palette, unifrog_palettes, reverse = reverse, ...)
+    palette_list <- c(unifrog_palettes,cb_palettes)
+    names(palette_list) <-
+      names(palette_list) |> stringr::str_replace("(.*)\\n(.*)", "\\1")
+    .retrieve_palette(palette, palette_list, reverse = reverse, ...)
   }
-
 
 # helper -----------------------
 
